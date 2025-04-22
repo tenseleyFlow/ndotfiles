ea7607aa2aebcc48143dac26302a016a4699c58e
espadonne <espadonne@outlook.com>
2025-04-22 13:32:51 -0400

    bring back in keybinds

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -30,13 +30,13 @@ end
 
 -- <<TUNER-START>>
 -- @ui: slider(min=10, max=42, step=1) type=int
-config.font_size = 23
+config.font_size = 17
 -- @ui: slider(min=0.05, max=1.0, step=0.01) type=float
-config.window_background_opacity = 0.36
--- @ui: select(options="Gruvbox Dark, Gruvbox Light, Catppuccin Mocha") type=string
-config.color_scheme = "Catppuccin Mocha"
+config.window_background_opacity = 0.05
+-- @ui: select(options="Aurora, Arthur, BirdsOfParadise, Bamboo, Ciapre, desert, Django, duskfox, Earthsong, Ef-Melissa-Dark, Elemental") type=string
+config.color_scheme = "Django"
 -- @ui: slider(min=1, max=100, step=1) type=int
-config.macos_window_background_blur = 24
+config.macos_window_background_blur = 15
 -- @ui: numerical(min=0.5, max=5.5, step=0.01) type=float
 config.line_height = 1.06
 -- <<TUNER-END>>
@@ -51,7 +51,7 @@ config.enable_scroll_bar = true
 config.colors = config.colors or {}
 config.colors.background = "#333333"
 config.window_decorations = "RESIZE"
-config.pane_focus_follows_mouse = true
+config.pane_focus_follows_mouse = false
 config.native_macos_fullscreen_mode = true
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font("JetBrains Mono")
@@ -73,31 +73,204 @@ end)
 -- :::: KEYBINDS :: my onetrue ::::
 -- ::::: :::::::::::::::::::::: :::::
 -- 
+
+config.key_tables = {
+  resize = {
+    { key="Escape",     action = "PopKeyTable" },
+    { key="UpArrow",    action = wezterm.action.AdjustPaneSize{"Up", 2} },
+    { key="DownArrow",  action = wezterm.action.AdjustPaneSize{"Down",2} },
+    { key="LeftArrow",  action = wezterm.action.AdjustPaneSize{"Left", 2} },
+    { key="RightArrow", action = wezterm.action.AdjustPaneSize{"Right", 2} },
+  },
+
+  utility = {
+    { key="c", action = wezterm.action.ActivateCopyMode },
+    { key="Escape", action = wezterm.action.ReloadConfiguration },
+    {
+      key = "b",
+      action = wezterm.action_callback(function(win)
+        local o = win:get_config_overrides() or {}
+        o.hide_tab_bar_if_only_one_tab = not o.hide_tab_bar_if_only_one_tab
+        win:set_config_overrides(o)
+      end),
+    },
+    {
+      key = "l", mods = "CMD|SHIFT",
+      action = wezterm.action_callback(function(win)
+        local o = win:get_config_overrides() or {}
+        o.enable_scroll_bar = not o.enable_scroll_bar
+        win:set_config_overrides(o)
+      end),
+    },
+  },
+
+  tabular = {
+    { key="n", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+    { key="RightArrow", action = wezterm.action.ActivateTabRelative(1) },
+    { key="LeftArrow", action = wezterm.action.ActivateTabRelative(-1) },
+    { key="w", action = wezterm.action.CloseCurrentTab { confirm = false } },
+  }
+}
+
+
 config.keys = {
+
+  -- :::
+  -- :::: MODALLY WE ROLL ALONG ::::
+  -- ::::: ::::::::::::::::::::: :::::
+  -- 
+
+  -- utility
+  {
+    key = "U",
+    mods = "CMD|SHIFT",
+    action = wezterm.action.ActivateKeyTable{
+      name = "utility",
+      one_shot = true
+    },
+  },
+
+  -- resize
   {
     key = "R",
-    mods = "CTRL|SHIFT",
-    action = wezterm.action.ReloadConfiguration,
+    mods = "CMD|SHIFT",
+    action = wezterm.action.ActivateKeyTable{
+      name = "resize",
+      one_shot = false
+    },
   },
+
+  -- tabs (mostly fallbacks)
+  {
+    key = "T",
+    mods = "CMD|SHIFT",
+    action = wezterm.action.ActivateKeyTable{
+      name = "tabular",
+      one_shot = false
+    },
+  },
+
+  
+  -- :::
+  -- :::: TABBY-THINGS ::::
+  -- ::::: :::::::::::: :::::
+  -- 
+
   {
     key = "W",
     mods = "CTRL|SHIFT",
-    action = wezterm.action.CloseCurrentTab { confirm = true },
+    action = wezterm.action.CloseCurrentTab { confirm = false },
   },
   {
     key = "T",
     mods = "CTRL|SHIFT",
     action = wezterm.action.SpawnTab("CurrentPaneDomain"),
   },
+
   {
-    key = "UpArrow",
-    mods = "CMD|OPT|CTRL",
+    key = "RightArrow",
+    mods = "CMD|ALT",
+    action = wezterm.action.ActivateTabRelative(1),
+  },
+  {
+    key = "Tab",
+    mods = "CTRL",
+    action = wezterm.action.ActivateTabRelative(1),
+  },
+  {
+    key = "LeftArrow",
+    mods = "CMD|ALT",
+    action = wezterm.action.ActivateTabRelative(-1),
+  },
+  {
+    key = "Tab",
+    mods = "SHIFT|CTRL",
+    action = wezterm.action.ActivateTabRelative(-1),
+  },
+
+  -- :::
+  -- :::: PANEY-THINGS ::::
+  -- ::::: :::::::::::: :::::
+  -- 
+
+  -- creation/deltion
+  {
+    key = "X",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+  {
+    key = "\\",
+    mods = "CMD",
     action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" },
   },
   {
-    key = "RightArrow",
-    mods = "CMD|OPT|CTRL",
+    key = "\\",
+    mods = "CMD|SHIFT",
     action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" },
+  },
+
+  -- navigation..
+  -- these are just keybinds
+  -- I've always liked for nav 
+  -- within application 'panes' != tabs!
+  {
+    key = "LeftArrow",
+    mods = "CMD|CTRL|ALT",
+    action = wezterm.action.ActivatePaneDirection("Left"),
+  },
+  {
+    key = "RightArrow",
+    mods = "CMD|CTRL|ALT",
+    action = wezterm.action.ActivatePaneDirection("Right"),
+  },
+  {
+    key = "UpArrow",
+    mods = "CMD|CTRL|ALT",
+    action = wezterm.action.ActivatePaneDirection("Up"),
+  },
+  {
+    key = "DownArrow",
+    mods = "CMD|CTRL|ALT",
+    action = wezterm.action.ActivatePaneDirection("Down"),
+  },
+
+  -- maximize/minimize pane
+  -- requires overriding the macOS default
+  {
+    key = 'm',
+    mods = 'CMD',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+
+  {
+    key = "m",
+    mods = "CMD",
+    action = wezterm.action.TogglePaneZoomState,
+  },
+
+
+  -- :::
+  -- :::: VISUAL ::::
+  -- ::::: :::::: :::::
+  --  
+
+  -- hi/lo opacity toggle
+  -- note this does not writeback
+  -- to the wezztershier tuning section
+  {
+    key = "o",
+    mods = "CMD",
+    action = wezterm.action_callback(function(window, pane)
+      local overrides = window:get_config_overrides() or {}
+      local opa = overrides.window_background_opacity
+      if opa == nil or opa == 1.0 then
+        overrides.window_background_opacity = 0.05
+      else
+        overrides.window_background_opacity = 1.0
+      end
+      window:set_config_overrides(overrides)
+    end),
   },
 }
 
