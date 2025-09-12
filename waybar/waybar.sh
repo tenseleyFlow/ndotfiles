bfcfb5c7bc8545a36307dc772e533d1196681ae9
mfwolffe <wolffemf@dukes.jmu.edu>
2025-09-12 18:25:09 -0400

    third screen in portrait

--- waybar/waybar.sh
+++ waybar/waybar.sh
@@ -6,5 +6,8 @@ killall -q waybar
 # Wait until the processes have been shut down
 while pgrep -x waybar >/dev/null; do sleep 1; done
 
-# Launch main
-waybar &
+# Launch waybar for landscape monitors
+waybar -c ~/.config/waybar/config-landscape &
+
+# Launch waybar for portrait monitor
+waybar -c ~/.config/waybar/config-portrait &
